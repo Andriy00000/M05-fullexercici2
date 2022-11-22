2a6cad93a92f042f8d29d0abf3b6fd002cb746b0
Mister Andriy <Mister Andriy@DESKTOP-B6PDHAC>
2022-11-22 17:36:24 +0100

    He ampliat el codi per crear un menu

--- src/main/java/com/mycompany/conversio/Conversio.java
+++ src/main/java/com/mycompany/conversio/Conversio.java
@@ -14,12 +14,29 @@ import java.util.Scanner;
 public class Conversio {
 
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Introdueix en €");
+        System.out.println("Selecciona 1 per passar d'Euro a Lliura");
+        
+        System.out.println("Selecciona 2 per passar de Lliura a Euro");
+        
+        int seleccio = sc.nextInt();
+        
+        if (seleccio == 1) {
+            System.out.println("Introdueix en €");
         int valor_euros = sc.nextInt();
                 
-        double tipus_de_canvi = 0.87; //lliura esterlina
-        System.out.println(valor_euros + " euros (€) actualment equivalen a" + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+        double tipus_de_canvi = 0.87d; //lliura esterlina
+        System.out.println(valor_euros + " euros (€) actualment equivalen a " + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+        }
+        if (seleccio == 2) {
+            System.out.println("Introdueix en £");
+        int valor_lliures = sc.nextInt();
+                
+        double tipus_de_canvi2 = 1.14d; //euros
+        System.out.println(valor_lliures + " lliures (£) actualment equivalen a " + (valor_lliures * tipus_de_canvi2) + " euros (€)");
+        }
+        
     }
 }
